11bf12ac3cccfe6992a42492e00ae5cf21604f90
BassManLuke <luke_haz@yahoo.co.uk>
2025-02-06 13:29:48 +0000

    create basic layout for Plant List

--- src/components/PlantsList.tsx
+++ src/components/PlantsList.tsx
@@ -1,42 +1,102 @@
-import { View, Text, FlatList, StyleSheet, Image } from 'react-native'
-import { fetchPlants } from '../api'
+import { Pressable, Text, FlatList, StyleSheet, Image, ActivityIndicator, View } from 'react-native'
+import { fetchUsersPlants } from '../api'
 import { useEffect, useState } from 'react'
-import { Plant } from '../types/plants'
+import { UserPlant } from '../types/plants'
+import colours from '../config/colours'
 
 const PlantsList = () => {
 
-    const [plantList, setPlantList] = useState<Plant[]>([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [plantList, setPlantList] = useState<UserPlant[]>([])
     
     useEffect(() => {
-        async function loadPlants() {
-        const plantListFromApi = await fetchPlants()
-        setPlantList(plantListFromApi.plants)
+        async function loadUserPlants() {
+            const plantListFromApi = await fetchUsersPlants(5) // Hard coded User for now
+            setPlantList(plantListFromApi.plants)
         }
-        loadPlants()
+        loadUserPlants()
     }, [])
+
+    function handlePress () {
+        console.log("Hello")
+    }
 
     return (
         <FlatList
-        data={plantList}
-        renderItem={({item}) => {
-            return (
-            <View>
-                <Text>{item.plant_name}</Text>
-                <Text>{item.about_plant}</Text>
-                <Image
-                style={{width: 200, height: 200}}
-                source={{
-                    uri: item.plant_image_url,
-                }}/>
-                <Text>{item.season}</Text>
-                <Text>{`\n`}</Text>
-            </View>
-            )
-        }}
+            contentContainerStyle={styles.list}
+            data={plantList}
+            keyExtractor={(item) => item.plant_id.toString()}
+            numColumns={2}
+            renderItem={({item}) => {
+                return (
+                    <Pressable 
+                        style={item.find_amount === 0 ? styles.dark : styles.plantCard}
+                        onPress={() => {
+                            handlePress()
+                        }}>
+                        <Image
+                            style={styles.image}
+                            source={{
+                                uri: item.plant_image_url,
+                            }}
+                            />
+                        <Text style={styles.cardTitle}>{item.plant_name}</Text>
+                        {/* <Text style={styles.text}>Appears in: {"\n"}{item.season}</Text> */}
+                        {/* <View style={styles.foundText}> */}
+                            <Text style={styles.foundText}>{item.find_amount === 0 ? `Not Found` : `Found: ${item.find_amount}`}</Text>
+                        {/* </View> */}
+                        <Text>{`\n`}</Text>
+                    </Pressable>
+                )
+            }}
         />
     )
 }
 
 export default PlantsList
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    list: {
+        alignSelf: "center",
+        alignItems: "center",
+    },
+    plantCard: {
+        position: 'relative',
+        borderRadius: 15,
+        backgroundColor: colours.bgHighlight,
+        alignItems: "center",
+        margin: 8,
+        padding: 7,
+        alignSelf: "center",
+    },
+    // I'm sure theres a better way to do this...
+    dark: {
+        position: 'relative',
+        borderRadius: 15,
+        backgroundColor: "grey",
+        alignItems: "center",
+        margin: 8,
+        padding: 7,
+        alignSelf: "center",
+    },
+    image: {
+        borderWidth: 3,
+        borderRadius: 10,
+        width: 150,
+        height: 150,
+    },
+    cardTitle: {
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    text: {
+        textAlign: "center",
+    },
+    foundText: {
+        textAlign: "right",
+        alignSelf: "flex-end",
+        position: 'absolute',
+        bottom: 7,
+        right: 7,
+    }
+})
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,18 +1,19 @@
 import { StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import SvgHome from '../../assets/tab-icons/tab-icon-components/Home';
 
-import HomeStack from './HomeStack';
-import PlantListStack from './PlantListStack';
-import FindAPlantStack from './FindAPlantStack';
-import ChatStack from './ChatStack';
-import MyProfileStack from './MyProfileStack';
-import colours from '../config/colours';
-import SvgListSl from '../../assets/tab-icons/tab-icon-components/ListSl';
-import SvgFind from '../../assets/tab-icons/tab-icon-components/Find';
-import SvgChat from '../../assets/tab-icons/tab-icon-components/Chat';
-import SvgUser from '../../assets/tab-icons/tab-icon-components/User';
+import HomeStack from './HomeStack'
+import PlantListStack from './PlantListStack'
+import FindAPlantStack from './FindAPlantStack'
+import ChatStack from './ChatStack'
+import MyProfileStack from './MyProfileStack'
+import colours from '../config/colours'
+
+import SvgHome from '../../assets/tab-icons/tab-icon-components/Home'
+import SvgListBl from '../../assets/tab-icons/tab-icon-components/ListBl'
+import SvgFind from '../../assets/tab-icons/tab-icon-components/Find'
+import SvgChat from '../../assets/tab-icons/tab-icon-components/Chat'
+import SvgUser from '../../assets/tab-icons/tab-icon-components/User'
 
 const Tab = createBottomTabNavigator()
 
@@ -37,7 +38,7 @@ const AppNavigator = () => {
             <Tab.Screen name="PlantList" component={PlantListStack}
             options={{
                 tabBarIcon: ({ color }) => {
-                    return <SvgListSl color={color} size={45} />
+                    return <SvgListBl color={color} size={45} />
                 },
                 headerShown: false,
             }}/>
@@ -72,12 +73,3 @@ const AppNavigator = () => {
 }
 
 export default AppNavigator
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#000000',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
